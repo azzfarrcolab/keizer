0a2197d474465a621a442799eee209d1cfe6a69a
azzfarrcolab <72619602+azzfarrcolab@users.noreply.github.com>
2020-10-27 05:02:13 -0700

    Add files via upload

---
+++ start_xmr.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+reset
+
+./SRBMiner-MULTI --algorithm randomx --pool pool.supportxmr.com:80 --wallet 42Ld5A5ttZn546fsgknM7e7TWJfEcpsUdcCko1asFFzMhGQkADe4Gt49HLUTeM51jG8xZdjxtgT67XpAktDEB15SP5De3ru --password rig01 --cpu-threads 0 --disable-gpu --log-file ./Logs/log-xmr.txt
